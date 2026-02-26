7333337d52ee5ece999d820e37d13eddc337e554
RafalMagrys <ra.magrys@gmail.com>
2026-02-26 21:03:15 +0100

    cr fixes

---
+++ script/utils/DeployUtils.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+// solhint-disable use-natspec
+pragma solidity =0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract DeployUtils is Script {
+    using stdJson for string;
+
+    function save(string memory json) internal {
+        string memory base = string.concat("./deployments/", network());
+
+        vm.createDir(base, true);
+        vm.writeJson(json, string.concat(base, "/latest.json"));
+        vm.writeJson(json, string.concat(base, "/", vm.toString(block.number), ".json"));
+    }
+
+    function createProxy(bytes memory init, address impl) internal returns (address proxy) {
+        proxy = address(new ERC1967Proxy(address(impl), init));
+    }
+
+    function network() internal view returns (string memory) {
+        if (block.chainid == 31415926) return "devnet";
+        else if (block.chainid == 314159) return "calibnet";
+        else if (block.chainid == 314) return "mainnet";
+        else return vm.toString(block.chainid);
+    }
+}
