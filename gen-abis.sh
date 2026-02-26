3e6d74989d7be219274112fcbb1a88e4f0ed27cc
RafalMagrys <ra.magrys@gmail.com>
2026-02-26 21:06:17 +0100

    abis and solhint

--- gen-abis.sh
+++ gen-abis.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 rm -rf abis
 mkdir abis
 
-for i in $(find src -name '*.sol' ! -path "*/interfaces/*"); do
+for i in $(find src -name '*.sol' ! -path "*/interfaces/*" ! -path "*/types/*"); do
   echo "Generating ABI file for $i"
   i="$(basename "$i")"
   name="${i%.sol}"
