617fd5f98b0eb4af78be4c25eeae284e02833ccb
RafalMagrys <ra.magrys@gmail.com>
2026-03-23 12:02:31 +0100

    fmt

--- test/contracts/ValidatorMock.sol
+++ test/contracts/ValidatorMock.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 // solhint-disable
 
-import {CommonTypes} from "filecoin-solidity/v0.8/types/CommonTypes.sol";
-
 pragma solidity =0.8.30;
+
+import {CommonTypes} from "filecoin-solidity/v0.8/types/CommonTypes.sol";
 
 contract ValidatorMock {
     function updateLockupPeriod(uint256 railId, uint256 newLockupPeriod) external {
